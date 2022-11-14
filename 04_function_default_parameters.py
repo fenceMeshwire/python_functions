842a7ff37107150d598d895399141ed91f6ea243
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-11-14 12:34:46 +0100

    Update 04_function_default_parameters.py

--- 04_function_default_parameters.py
+++ 04_function_default_parameters.py
@@ -8,13 +8,13 @@
 
 def get_part(id, partname, qty=1):
     if qty == 1:
-        query = f"Get {qty} unit from part {partname} with {id}."
+        query = f"Get {qty} unit from part '{partname}' with {id}."
     else:
-        query = f"Get {qty} units from part {partname} with {id}."
+        query = f"Get {qty} units from part '{partname}' with {id}."
     return query
 
 if __name__ == '__main__':
-    partname = 'washers'
+    partname = 'washer'
     id = 1723
     # Quantity qty is not specified, so the function 
     # assigns qty=1 automatically
